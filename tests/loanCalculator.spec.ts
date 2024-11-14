d197b8eeea79776c6ea5fb8032fcb9e44a020bc6
AngpeuK <angpeu@gmail.com>
2024-11-14 03:01:10 +0200

    Update loanCalculator.spec.ts
    Final fixes for Perfect solution

--- tests/loanCalculator.spec.ts
+++ tests/loanCalculator.spec.ts
@@ -63,7 +63,7 @@ test.describe('API tests for risk calculation', () => {
       } else {
         // Print if Undocumented Risk Level
         console.warn(
-          `Unexpected risk level, so no period provided, that is ok: ${responseBody.riskLevel}`,
+          `1.2/1.3/1.4 Unexpected risk level, so no period provided, that is ok: ${responseBody.riskLevel}`,
         )
       }
     })
@@ -93,12 +93,80 @@ test.describe('API tests for risk calculation', () => {
       if (responseBody.riskLevel === 'Low Risk') {
         expect.soft(responseBody.riskPeriods).toEqual(expect.arrayContaining([12, 18, 24, 30, 36]))
       } else if (responseBody.riskLevel === 'Medium Risk') {
-        console.warn(`inadequate risk level: ${responseBody.riskLevel}`)
+        console.warn(`1.2 inadequate risk level: ${responseBody.riskLevel}`)
       } else if (responseBody.riskLevel === 'High Risk') {
-        console.warn(`inadequate risk level: ${responseBody.riskLevel}`)
+        console.warn(`1.2 inadequate risk level: ${responseBody.riskLevel}`)
       } else {
         // Print if Undocumented Risk Level
-        console.warn(`inadequate risk level: ${responseBody.riskLevel}`)
+        console.warn(`1.2 inadequate risk level: ${responseBody.riskLevel}`)
+      }
+    })
+
+    test('1.3 Medium Risk Score loanPeriod return should be adequate, should return status code 200', async ({
+      request,
+    }) => {
+      const requestBody = ApplicationDto.createMediumRiskApplication()
+      // console.log('requestBody for 1.3:', requestBody) //Print Request Body
+      //Request
+      const response = await request.post(RequestURL, {
+        data: requestBody,
+      })
+      expect(response.status()).toBe(StatusCodes.OK)
+      //Response
+      const responseBody = await response.json()
+      // Verify all fields in response body
+      expect.soft(responseBody.riskScore).toBeDefined()
+      expect.soft(responseBody.riskLevel).toBeDefined()
+      expect.soft(responseBody.riskPeriods).toBeDefined()
+      expect.soft(responseBody.applicationId).toBeDefined()
+      expect.soft(responseBody.riskDecision).toBeDefined()
+      //Print Response Body and status code
+      // console.log('ResponseBody for 1.3:', responseBody)
+      // console.log('Response status code for 1.3:', response.status())
+
+      if (responseBody.riskLevel === 'Medium Risk') {
+        expect.soft(responseBody.riskPeriods).toEqual(expect.arrayContaining([6, 9, 12]))
+      } else if (responseBody.riskLevel === 'Low Risk') {
+        console.warn(`1.3 inadequate risk level: ${responseBody.riskLevel}`)
+      } else if (responseBody.riskLevel === 'High Risk') {
+        console.warn(`1.3 inadequate risk level: ${responseBody.riskLevel}`)
+      } else {
+        // Print if Undocumented Risk Level
+        console.warn(`1.3 inadequate risk level: ${responseBody.riskLevel}`)
+      }
+    })
+
+    test('1.4 Medium Risk Score loanPeriod return should be adequate, should return status code 200', async ({
+      request,
+    }) => {
+      const requestBody = ApplicationDto.createHighRiskApplication()
+      // console.log('requestBody for 1.4:', requestBody) //Print Request Body
+      //Request
+      const response = await request.post(RequestURL, {
+        data: requestBody,
+      })
+      expect(response.status()).toBe(StatusCodes.OK)
+      //Response
+      const responseBody = await response.json()
+      // Verify all fields in response body
+      expect.soft(responseBody.riskScore).toBeDefined()
+      expect.soft(responseBody.riskLevel).toBeDefined()
+      expect.soft(responseBody.riskPeriods).toBeDefined()
+      expect.soft(responseBody.applicationId).toBeDefined()
+      expect.soft(responseBody.riskDecision).toBeDefined()
+      //Print Response Body and status code
+      // console.log('ResponseBody for 1.4:', responseBody)
+      // console.log('Response status code for 1.4:', response.status())
+
+      if (responseBody.riskLevel === 'High Risk') {
+        expect.soft(responseBody.riskPeriods).toEqual(expect.arrayContaining([3, 6]))
+      } else if (responseBody.riskLevel === 'Medium Risk') {
+        console.warn(`1.4 inadequate risk level: ${responseBody.riskLevel}`)
+      } else if (responseBody.riskLevel === 'Low Risk') {
+        console.warn(`1.4 inadequate risk level: ${responseBody.riskLevel}`)
+      } else {
+        // Print if Undocumented Risk Level
+        console.warn(`1.4 inadequate risk level: ${responseBody.riskLevel}`)
       }
     })
   })
